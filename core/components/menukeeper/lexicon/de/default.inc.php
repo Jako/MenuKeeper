d343961149c03017f4625d27673dd59e6077240a
Thomas Jakobi <thomas.jakobi@partout.info>
2022-12-31 01:26:19 +0100

    Lexicon changes

--- core/components/menukeeper/lexicon/de/default.inc.php
+++ core/components/menukeeper/lexicon/de/default.inc.php
@@ -8,10 +8,10 @@
 $_lang['menukeeper'] = 'MenuKeeper';
 $_lang['menukeeper.menu'] = 'MenuKeeper';
 $_lang['menukeeper.menu_desc'] = 'Die Reihenfolge des Manager-Menüs beibehalten';
-$_lang['menukeeper.menu_load'] = 'MenuKeeper Laden';
+$_lang['menukeeper.menu_load'] = 'Laden';
 $_lang['menukeeper.menu_load_confirm'] = 'Sind Sie sicher, dass Sie die gespeicherten Menüeinträge laden wollen? Der Manager wird danach neu geladen.';
 $_lang['menukeeper.menu_load_desc'] = 'Menüeinträge laden';
 $_lang['menukeeper.menu_loaded'] = 'Menüeinträge geladen!';
-$_lang['menukeeper.menu_save'] = 'MenuKeeper Speichern';
+$_lang['menukeeper.menu_save'] = 'Speichern';
 $_lang['menukeeper.menu_save_desc'] = 'Menüeinträge speichern';
 $_lang['menukeeper.menu_saved'] = 'Menüeinträge gespeichert!';
