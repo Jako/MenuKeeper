793ead92adbf31c1ebf85af2096dcf30c0a5379b
Thomas Jakobi <thomas.jakobi@partout.info>
2025-03-09 17:32:39 +0100

    Fix a regular expression detecting the version in the main class

--- gulpfile.js
+++ gulpfile.js
@@ -52,7 +52,7 @@ const bumpVersion = function () {
     return gulp.src([
         'core/components/menukeeper/src/MenuKeeper.php',
     ], {base: './'})
-        .pipe(replace(/version = '\d+\.\d+\.\d+[-a-z0-9]*'/ig, 'version = \'' + pkg.version + '\''))
+        .pipe(replace(/version = '\d+\.\d+\.\d+-?[0-9a-z]*'/ig, 'version = \'' + pkg.version + '\''))
         .pipe(gulp.dest('.'));
 };
 const bumpDocs = function () {
